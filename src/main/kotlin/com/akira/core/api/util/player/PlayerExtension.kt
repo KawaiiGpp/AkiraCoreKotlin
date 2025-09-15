e8136dab062d70d4c63d1945c1029f7defe4e79d
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-09-15 10:31:49 +0800

    优化PlayerExtension的排版。

--- src/main/kotlin/com/akira/core/api/util/player/PlayerExtension.kt
+++ src/main/kotlin/com/akira/core/api/util/player/PlayerExtension.kt
@@ -7,27 +7,29 @@ import org.bukkit.Sound
 import org.bukkit.entity.Player
 import java.time.Duration
 
-fun Player.playFollowingSound(sound: Sound, pitch: Float = 1.0F, volume: Float = 0.5F) {
-    this.playSound(this, sound, volume, pitch)
-}
+fun Player.playFollowingSound(
+    sound: Sound,
+    pitch: Float = 1.0F,
+    volume: Float = 0.5F
+) = this.playSound(this, sound, volume, pitch)
 
-fun Player.playEnvironmentSound(sound: Sound, pitch: Float = 1.0F, volume: Float = 0.5F) {
-    this.playSound(this.location, sound, volume, pitch)
-}
+fun Player.playEnvironmentSound(
+    sound: Sound,
+    pitch: Float = 1.0F,
+    volume: Float = 0.5F
+) = this.playSound(this.location, sound, volume, pitch)
 
 fun Player.showTitle(
     main: Component, sub: Component = Component.empty(),
     fadeIn: Long = 10, duration: Long = 80, fadeOut: Long = 10
-) {
-    this.showTitle(
-        Title.title(
-            main,
-            sub,
-            Times.times(
-                Duration.ofMillis(fadeIn * 50),
-                Duration.ofMillis(duration * 50),
-                Duration.ofMillis(fadeOut * 50)
-            )
+) = this.showTitle(
+    Title.title(
+        main,
+        sub,
+        Times.times(
+            Duration.ofMillis(fadeIn * 50),
+            Duration.ofMillis(duration * 50),
+            Duration.ofMillis(fadeOut * 50)
         )
     )
-}
+)
