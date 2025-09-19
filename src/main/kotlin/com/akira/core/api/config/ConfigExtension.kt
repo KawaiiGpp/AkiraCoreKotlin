0ad417770d2db37702adfb0b052f09e97c470865
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-09-19 10:28:40 +0800

    新增ConfigExtension类。

---
+++ src/main/kotlin/com/akira/core/api/config/ConfigExtension.kt
@@ -0,0 +1,16 @@
+package com.akira.core.api.config
+
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.configuration.file.YamlConfiguration
+
+fun YamlConfiguration.getWorld(path: String): World? {
+    val name = this.getString(path) ?: return null
+    return Bukkit.getWorld(name)
+}
+
+fun YamlConfiguration.getLocationList(path: String): List<Location>? {
+    val section = this.getConfigurationSection(path) ?: return null
+    return section.getKeys(false).mapNotNull(this::getLocation)
+}
