fb29801b7ff4d9730ed9c1353ea69172ccb9de73
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-09-26 21:37:48 +0800

    优化了ConfigSerializer的形参命名。

--- src/main/kotlin/com/akira/core/api/config/ConfigSerializer.kt
+++ src/main/kotlin/com/akira/core/api/config/ConfigSerializer.kt
@@ -3,7 +3,7 @@ package com.akira.core.api.config
 import org.bukkit.configuration.ConfigurationSection
 
 interface ConfigSerializer<T : Any> {
-    fun serialize(value: T, config: ConfigurationSection)
+    fun serialize(value: T, section: ConfigurationSection)
 
-    fun deserialize(config: ConfigurationSection): T?
+    fun deserialize(section: ConfigurationSection): T?
 }
