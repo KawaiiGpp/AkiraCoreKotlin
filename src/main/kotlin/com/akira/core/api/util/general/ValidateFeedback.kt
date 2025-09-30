7a65679fb9a38fcc00b0631268b35007a0476245
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-09-30 16:24:53 +0800

    新增ValidateFeedback方法。

---
+++ src/main/kotlin/com/akira/core/api/util/general/ValidateFeedback.kt
@@ -0,0 +1,13 @@
+package com.akira.core.api.util.general
+
+class ValidateFeedback {
+    private val list = mutableListOf<String>()
+    val content get() = list.toList()
+
+    val passed get() = list.isEmpty()
+    val failed get() = list.isNotEmpty()
+
+    fun add(line: String) = list.add(line)
+
+    fun add(lines: Collection<String>) = list.addAll(lines)
+}
