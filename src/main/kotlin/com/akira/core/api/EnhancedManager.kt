e1cc6e9ca027547550143ec4da0d5608dc36770c
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-09-30 13:02:48 +0800

    删除了EnhancedManager<E>的unregister方法。

--- src/main/kotlin/com/akira/core/api/EnhancedManager.kt
+++ src/main/kotlin/com/akira/core/api/EnhancedManager.kt
@@ -3,7 +3,5 @@ package com.akira.core.api
 abstract class EnhancedManager<E> : Manager<String, E>() {
     open fun register(element: E) = super.register(transform(element), element)
 
-    open fun unregister(element: E) = super.unregister(transform(element))
-
     protected abstract fun transform(element: E): String
 }
