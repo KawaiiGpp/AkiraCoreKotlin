d3882575a123ca63a505fecb4145504a73824154
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-10-14 11:14:18 +0800

    删除ConfigSerializable的泛型参数。

--- src/main/kotlin/com/akira/core/api/config/ConfigSerializable.kt
+++ src/main/kotlin/com/akira/core/api/config/ConfigSerializable.kt
@@ -2,7 +2,7 @@ package com.akira.core.api.config
 
 import org.bukkit.configuration.ConfigurationSection
 
-interface ConfigSerializable<T : Any> {
+interface ConfigSerializable {
     fun serialize(section: ConfigurationSection)
 
     fun deserialize(section: ConfigurationSection)
