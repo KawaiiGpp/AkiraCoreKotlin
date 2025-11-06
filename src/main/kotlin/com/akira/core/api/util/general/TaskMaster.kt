9fd2e651176c059e042f41ca14e6d0d5ca1fb42c
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-11-06 23:09:18 +0800

    修改TaskMaster中的run字样为launch避免冲突。

--- src/main/kotlin/com/akira/core/api/util/general/TaskMaster.kt
+++ src/main/kotlin/com/akira/core/api/util/general/TaskMaster.kt
@@ -5,20 +5,20 @@ import org.bukkit.Bukkit
 
 private val scheduler get() = Bukkit.getScheduler()
 
-fun AkiraPlugin.run(block: () -> Unit) =
+fun AkiraPlugin.launch(block: () -> Unit) =
     scheduler.runTask(this, Runnable(block))
 
-fun AkiraPlugin.run(later: Long, block: () -> Unit) =
+fun AkiraPlugin.launch(later: Long, block: () -> Unit) =
     scheduler.runTaskLater(this, Runnable(block), later)
 
-fun AkiraPlugin.run(later: Long, period: Long, block: () -> Unit) =
+fun AkiraPlugin.launch(later: Long, period: Long, block: () -> Unit) =
     scheduler.runTaskTimer(this, Runnable(block), later, period)
 
-fun AkiraPlugin.runAsync(block: () -> Unit) =
+fun AkiraPlugin.launchAsync(block: () -> Unit) =
     scheduler.runTaskAsynchronously(this, Runnable(block))
 
-fun AkiraPlugin.runAsync(later: Long, block: () -> Unit) =
+fun AkiraPlugin.launchAsync(later: Long, block: () -> Unit) =
     scheduler.runTaskLaterAsynchronously(this, Runnable(block), later)
 
-fun AkiraPlugin.runAsync(later: Long, period: Long, block: () -> Unit) =
+fun AkiraPlugin.launchAsync(later: Long, period: Long, block: () -> Unit) =
     scheduler.runTaskTimerAsynchronously(this, Runnable(block), later, period)
