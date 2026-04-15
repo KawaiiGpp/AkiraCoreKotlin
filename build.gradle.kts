db4ccdeedf65db11b10451d7f2d417e6d378c09f
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-04-15 11:44:14 +0800

    重构：完全重构Gradle构建脚本。改用Kotlin DSL。

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import io.papermc.paperweight.tasks.RemapJar
+
+plugins {
+    `maven-publish`
+    kotlin("jvm") version "2.1.20"
+    id("com.gradleup.shadow") version "8.3.10"
+    id("io.papermc.paperweight.userdev") version "1.7.1"
+}
+
+group = "com.akira"
+version = "1.3.7"
+
+val serverFolder = "D:/Server/PaperDebug"
+val pluginFolder = "$serverFolder/plugins"
+
+java { withSourcesJar() }
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.20.6-R0.1-SNAPSHOT")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("shadow")
+    dependencies { exclude(dependency("org.jetbrains:annotations")) }
+    relocate("kotlin", "com.akira.shadow.kotlin")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks.assemble { dependsOn("reobfJar") }
+
+tasks.register<Copy>("deployPlugin") {
+    dependsOn("reobfJar")
+
+    from(tasks.named<RemapJar>("reobfJar").flatMap { it.outputJar })
+    into(pluginFolder)
+    rename { "${project.name}.jar" }
+}
+
+tasks.build { dependsOn("deployPlugin") }
+
+tasks.named("publishToMavenLocal") { finalizedBy("deployPlugin") }
+
+tasks.withType<ProcessResources> {
+    inputs.property("checkVersion", project.version)
+    filesMatching("plugin.yml") { expand("version" to project.version) }
+}
+
+tasks.withType<Test> { useJUnitPlatform() }
+
+kotlin { jvmToolchain(21) }
