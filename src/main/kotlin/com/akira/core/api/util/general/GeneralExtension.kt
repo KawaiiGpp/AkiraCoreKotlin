0fde237dee14ac9474edf103ec394b2f098bff04
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-04-19 12:10:51 +0800

    补充GeneralExtension中的文档并优化逻辑。

--- src/main/kotlin/com/akira/core/api/util/general/GeneralExtension.kt
+++ src/main/kotlin/com/akira/core/api/util/general/GeneralExtension.kt
@@ -5,18 +5,23 @@ import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityDamageEvent.DamageModifier
 import kotlin.math.min
 
+/**
+ * 无效化事件中的减伤修饰符。
+ *
+ * 当所有减伤算法失效，扣血行为类似于真伤逻辑，默认不绕过伤害吸收。
+ *
+ * @param bypassAbsorption 是否使伤害吸收失效，默认为 `false`
+ */
 @Suppress("DEPRECATION")
 fun EntityDamageEvent.bypassVanillaModifiers(bypassAbsorption: Boolean = false) {
+    val entity = this.entity as? LivingEntity
     val modifiers = DamageModifier.entries
+        .asSequence()
         .filter { it != DamageModifier.BASE }
         .filter { this.isApplicable(it) }
 
     for (modifier in modifiers) {
-        if (modifier == DamageModifier.ABSORPTION && !bypassAbsorption) {
-            val entity = this.entity as? LivingEntity
-            entity?.let { setDamage(modifier, -min(it.absorptionAmount, damage)) }
-        } else {
-            this.setDamage(modifier, 0.0)
-        }
+        if (modifier != DamageModifier.ABSORPTION || bypassAbsorption) this.setDamage(modifier, 0.0)
+        else entity?.let { setDamage(modifier, -min(it.absorptionAmount, damage)) }
     }
 }
