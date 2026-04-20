ccd02b300956b52b9341f2e597081c1cd20b4d0d
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-04-20 14:37:13 +0800

    为EntityExtension新增文档并改善逻辑。

--- src/main/kotlin/com/akira/core/api/util/entity/EntityExtension.kt
+++ src/main/kotlin/com/akira/core/api/util/entity/EntityExtension.kt
@@ -3,29 +3,55 @@ package com.akira.core.api.util.entity
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
 import org.bukkit.attribute.AttributeInstance
-import org.bukkit.damage.DamageSource
-import org.bukkit.damage.DamageType
+import org.bukkit.attribute.AttributeModifier
 import org.bukkit.entity.LivingEntity
 
-fun LivingEntity.getBaseMaxHealth(): Double =
-    this.getNonNullAttribute(Attribute.GENERIC_MAX_HEALTH).baseValue
+/**
+ * 实体 [Attribute.GENERIC_MAX_HEALTH] 的属性实例
+ *
+ * @throws IllegalArgumentException 当实体不支持该属性
+ */
+val LivingEntity.maxHealthAttribute get() = this.requireAttribute(Attribute.GENERIC_MAX_HEALTH)
 
-fun LivingEntity.getFinalMaxHealth(): Double =
-    this.getNonNullAttribute(Attribute.GENERIC_MAX_HEALTH).value
+/**
+ * 实体的基础生命值上限
+ *
+ * 未经过 [AttributeModifier] 计算的基础值。
+ */
+var LivingEntity.baseMaxHealth
+    get() = maxHealthAttribute.baseValue
+    set(value) = value.let { maxHealthAttribute.baseValue = it }
 
-fun LivingEntity.setBaseMaxHealth(value: Double) =
-    value.let { this.getNonNullAttribute(Attribute.GENERIC_MAX_HEALTH).baseValue = it }
+/**
+ * 实体的最终生命值上限
+ *
+ * 经过 [AttributeModifier] 计算后的最终值。
+ */
+val LivingEntity.finalMaxHealth get() = maxHealthAttribute.value
 
-fun LivingEntity.resetMaxHealthModifiers() {
-    val attribute = this.getNonNullAttribute(Attribute.GENERIC_MAX_HEALTH)
-    attribute.modifiers.forEach { attribute.removeModifier(it) }
+/**
+ * 重置实体的生命值上限。
+ *
+ * 抹除其 [AttributeInstance] 中的所有 [AttributeModifier]。
+ */
+fun LivingEntity.clearMaxHealthModifiers() {
+    val modifiers = maxHealthAttribute.modifiers
+    modifiers.forEach(maxHealthAttribute::removeModifier)
 }
 
-fun LivingEntity.getNonNullAttribute(attribute: Attribute): AttributeInstance =
-    requireNotNull(this.getAttribute(attribute)) { "Attribute not supported for $type" }
-
-fun LivingEntity.dealTrueDamage(damage: Double) {
-    this.damage(damage, DamageSource.builder(DamageType.OUT_OF_WORLD).build())
+/**
+ * 安全获取实体的 [AttributeInstance]。
+ *
+ * @throws IllegalArgumentException 当实体不支持该属性
+ */
+fun LivingEntity.requireAttribute(attribute: Attribute): AttributeInstance {
+    val result = this.getAttribute(attribute)
+    return requireNotNull(result) { "Attribute $attribute not supported for $type." }
 }
 
+/**
+ * 实体是否举起盾牌
+ *
+ * 仅指示实体是否举盾，而非是否进入盾牌防御状态。
+ */
 val LivingEntity.isShieldRaised get() = isHandRaised && activeItem.type == Material.SHIELD
