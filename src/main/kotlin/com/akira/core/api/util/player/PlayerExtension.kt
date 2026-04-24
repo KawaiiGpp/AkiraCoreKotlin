981017e0085dfe3d47b408b89ccfa96eb7b28e7b
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-04-24 18:04:54 +0800

    为util/player下的类补充文档。

--- src/main/kotlin/com/akira/core/api/util/player/PlayerExtension.kt
+++ src/main/kotlin/com/akira/core/api/util/player/PlayerExtension.kt
@@ -1,22 +1,64 @@
 package com.akira.core.api.util.player
 
+import com.akira.core.api.util.math.requireNonNegative
+import com.akira.core.api.util.text.toComponent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.title.Title
 import net.kyori.adventure.title.Title.Times
 import org.bukkit.entity.Player
 import java.time.Duration
 
+/**
+ * 向玩家展示标题信息。
+ *
+ * - 若屏幕上正在展示另一组标题信息，将覆盖
+ * - [fadeIn]、[duration]、[fadeOut] 单位为游戏刻
+ *
+ * @throws IllegalArgumentException 当时长为负数
+ */
 fun Player.showTitle(
-    main: Component, sub: Component = Component.empty(),
-    fadeIn: Long = 10, duration: Long = 80, fadeOut: Long = 10
-) = this.showTitle(
-    Title.title(
-        main,
-        sub,
-        Times.times(
-            Duration.ofMillis(fadeIn * 50),
-            Duration.ofMillis(duration * 50),
-            Duration.ofMillis(fadeOut * 50)
+    title: Component,
+    subtitle: Component = Component.empty(),
+    fadeIn: Long = 10,
+    duration: Long = 80,
+    fadeOut: Long = 10
+) {
+    fadeIn.requireNonNegative("Fade-in ticks")
+    duration.requireNonNegative("Title ticks")
+    fadeOut.requireNonNegative("Fade-out ticks")
+
+    this.showTitle(
+        Title.title(
+            title,
+            subtitle,
+            Times.times(
+                Duration.ofMillis(fadeIn * 50),
+                Duration.ofMillis(duration * 50),
+                Duration.ofMillis(fadeOut * 50)
+            )
         )
     )
-)
+}
+
+/**
+ * 向玩家展示标题信息。
+ *
+ * - 自动将内容 [String] 转换为 [Component]
+ *
+ * @throws IllegalArgumentException 当时长为负数
+ */
+fun Player.showTitle(
+    title: String,
+    subtitle: String? = null,
+    fadeIn: Long = 10,
+    duration: Long = 80,
+    fadeOut: Long = 10
+) {
+    this.showTitle(
+        title.toComponent(),
+        subtitle?.toComponent() ?: Component.empty(),
+        fadeIn,
+        duration,
+        fadeOut
+    )
+}
--- src/main/kotlin/com/akira/core/api/util/player/PlayerUtils.kt
+++ src/main/kotlin/com/akira/core/api/util/player/PlayerUtils.kt
@@ -1,31 +1,37 @@
 package com.akira.core.api.util.player
 
 import com.akira.core.api.AkiraPlugin
+import com.akira.core.api.util.general.launch
+import com.akira.core.api.util.general.launchAsync
 import com.akira.core.api.util.general.separateUniqueId
 import com.akira.core.api.util.web.fetchHttp
 import com.google.gson.JsonParser
-import org.bukkit.Bukkit
 import java.util.*
 
+/**
+ * 通过 `Mojang API` 获取玩家的 [UUID]。
+ *
+ * - 以 [plugin] 的名义，启动异步拉取数据，避免了阻塞。
+ *
+ * @param callback 若成功获取 [UUID]，该回调执行
+ * @param exceptionSolver 若有异常，该回调执行
+ */
 fun fetchPlayerUniqueId(
-    plugin: AkiraPlugin, name: String,
-    exceptionSolver: (Throwable) -> Unit,
-    callback: (UUID) -> Unit
+    plugin: AkiraPlugin,
+    playerName: String,
+    callback: (UUID) -> Unit,
+    exceptionSolver: (Throwable) -> Unit
 ) {
-    val scheduler = Bukkit.getScheduler()
-
-    fun runSync(runnable: Runnable) {
-        scheduler.runTask(plugin, runnable)
-    }
-
-    scheduler.runTaskAsynchronously(plugin, Runnable {
-        runCatching {
-            val response = fetchHttp("https://api.mojang.com/users/profiles/minecraft/$name")
+    plugin.launchAsync {
+        val result = runCatching {
+            val url = "https://api.mojang.com/users/profiles/minecraft/$playerName"
+            val response = fetchHttp(url)
             val json = JsonParser.parseString(response).asJsonObject
 
             UUID.fromString(separateUniqueId(json.get("id").asString))
         }
-            .onSuccess { runSync { callback(it) } }
-            .onFailure { runSync { exceptionSolver(it) } }
-    })
+
+        result.onSuccess { plugin.launch { callback(it) } }
+        result.onFailure { plugin.launch { exceptionSolver(it) } }
+    }
 }
